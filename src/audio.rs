197eef2784a0619fcbfd52c8bb10982b39e5dd8d
Julian Bieber <julian.bieber.1992@gmail.com>
2023-05-11 00:09:53 +0200

    remove ringbuffer abstaction

--- src/audio.rs
+++ src/audio.rs
@@ -1,6 +1,5 @@
 use std::sync::{Arc, Mutex};
 use std::thread::JoinHandle;
-use std::time::Instant;
 
 use alsa::pcm::*;
 use alsa::{Direction, Error, ValueOr};
@@ -13,65 +12,26 @@ pub fn start(output: Arc<Mutex<Vec<f32>>>) -> JoinHandle<()> {
         let device = start_capture("default").unwrap();
         let io = device.io_i16().unwrap();
         let mut chunk_buffer = vec![0i16; length];
-        let mut ring_buffer = RingBuffer::new(length);
+        let mut complex_buffer = vec![Complex32 { re: 0.0, im: 0.0 }; length];
 
         let mut planner = FftPlanner::new();
         let fft = planner.plan_fft_forward(length);
         let scaling = length as f32;
         loop {
             io.readi(&mut chunk_buffer).unwrap();
-            ring_buffer.add(&chunk_buffer);
-            let mut c = ring_buffer.complex_vector();
-            fft.process(&mut c);
+            for (c, v) in complex_buffer.iter_mut().zip(chunk_buffer.iter()) {
+                c.re = *v as f32;
+                c.im = 0.0;
+            }
+
+            fft.process(&mut complex_buffer);
             let mut o = output.lock().unwrap();
-            o.iter_mut().zip(c.iter()).for_each(|(o, c)| {
+            o.iter_mut().zip(complex_buffer.iter()).for_each(|(o, c)| {
                 *o = c.re.sqrt() / scaling;
             });
             drop(o);
         }
     })
-}
-
-struct RingBuffer {
-    buffer: Vec<i16>,
-    current: usize,
-    c: Vec<Complex32>,
-}
-
-impl RingBuffer {
-    fn new(size: usize) -> Self {
-        RingBuffer {
-            buffer: vec![0; size],
-            current: 0,
-            c: Vec::with_capacity(size),
-        }
-    }
-
-    fn add(&mut self, chunk: &[i16]) {
-        for v in chunk {
-            self.buffer[self.current] = *v;
-            self.current += 1;
-            self.current %= self.buffer.len();
-        }
-    }
-
-    fn complex_vector(&mut self) -> &mut Vec<Complex32> {
-        let mut i = (self.current + 1) % self.buffer.len();
-        self.c.clear();
-        while i != self.current {
-            self.c.push(Complex32 {
-                re: self.buffer[i] as f32,
-                im: 0.0,
-            });
-            i += 1;
-            i %= self.buffer.len();
-        }
-        self.c.push(Complex32 {
-            re: self.buffer[i] as f32,
-            im: 0.0,
-        });
-        &mut self.c
-    }
 }
 
 fn start_capture(device: &str) -> Result<PCM, Error> {
