64bd75d7718b1466d28bbbf7dc441354943a926c
Julian Bieber <j.bieber@patagona.de>
2023-05-13 21:09:53 +0200

    refactor rendering to better accomodate ui

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ mod util;
 use std::{
     path::PathBuf,
     sync::{Arc, Mutex},
-    time::{Duration, Instant},
+    time::Instant,
 };
 
 use clap::Parser;
@@ -14,7 +14,7 @@ use model::{create_fft_buffer, create_time_buffer, Vertex};
 use util::Fps;
 use wgpu::{
     util::DeviceExt, Backends, BindGroup, BindGroupLayout, BindGroupLayoutDescriptor, Buffer,
-    Device,
+    Device, Queue, RenderPass, RenderPipeline, TextureFormat,
 };
 use winit::{
     event::*,
@@ -28,6 +28,8 @@ struct Opt {
     shader_path: PathBuf,
     #[arg(long)]
     srgb: bool,
+    #[arg(long)]
+    fps: bool,
 }
 
 fn main() {
@@ -37,10 +39,7 @@ fn main() {
 
     let opt = dbg!(Opt::parse());
 
-    let vertex_shader = include_str!("shader.vert");
     let fragment_shader = std::fs::read_to_string(opt.shader_path).unwrap();
-
-    let vertices = Vertex::square();
 
     // let mut scene = Scene::new(light_spheres, spheres);
     let event_loop = EventLoop::new();
@@ -57,16 +56,8 @@ fn main() {
         )))
         .build(&event_loop)
         .unwrap();
-    let mut state = pollster::block_on(State::new(
-        &window,
-        &vertices,
-        vertex_shader,
-        &fragment_shader,
-        &o,
-        opt.srgb,
-    ));
+    let mut state = pollster::block_on(State::new(&window, &fragment_shader, &o, opt.srgb));
     let mut fps = Fps::new();
-    let start = Instant::now();
     event_loop.run(move |event, _, control_flow| match event {
         Event::WindowEvent {
             ref event,
@@ -89,9 +80,11 @@ fn main() {
             _ => {}
         },
         Event::RedrawRequested(window_id) if window_id == window.id() => {
-            fps.presented();
-            // dbg!(fps.fps());
-            match state.render(start.elapsed(), &o) {
+            if opt.fps {
+                fps.presented();
+                dbg!(fps.fps());
+            }
+            match state.render() {
                 Ok(_) => {}
                 Err(wgpu::SurfaceError::Lost) => state.resize(state.size),
                 Err(wgpu::SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
@@ -113,20 +106,13 @@ struct State {
     queue: wgpu::Queue,
     config: wgpu::SurfaceConfiguration,
     size: winit::dpi::PhysicalSize<u32>,
-    render_pipeline: wgpu::RenderPipeline,
-    vertex_buffer: wgpu::Buffer,
-    bind_group: wgpu::BindGroup,
-    time_buffer: Buffer,
-    fft_buffer: Buffer,
-    num_vertices: usize,
+    main_display: MainDisplay,
 }
 
 impl State {
     // Creating some of the wgpu types requires async code
     async fn new(
         window: &Window,
-        vertices: &Vec<Vertex>,
-        vertex_shader_s: &str,
         fragment_shader_s: &str,
         fft: &Arc<Mutex<Vec<f32>>>,
         srgb: bool,
@@ -152,8 +138,7 @@ impl State {
         let surface_format = surface_caps
             .formats
             .iter()
-            .filter(|f| if srgb { f.is_srgb() } else { !f.is_srgb() })
-            .next()
+            .find(|f| if srgb { f.is_srgb() } else { !f.is_srgb() })
             .copied()
             .unwrap_or(surface_caps.formats[0]);
         let (device, queue) = adapter
@@ -178,11 +163,86 @@ impl State {
             view_formats: vec![],
         };
         surface.configure(&device, &config);
+        let main_display = MainDisplay::new(fft.clone(), &device, fragment_shader_s, config.format);
 
+        Self {
+            surface,
+            device,
+            queue,
+            config,
+            size,
+            main_display,
+        }
+    }
+
+    fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
+        if new_size.width > 0 && new_size.height > 0 {
+            self.size = new_size;
+            self.config.width = self.size.width;
+            self.config.height = self.size.height;
+            self.surface.configure(&self.device, &self.config);
+        }
+    }
+
+    fn input(&mut self, _event: &WindowEvent) -> bool {
+        false
+    }
+
+    fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
+        let output = self.surface.get_current_texture()?;
+        let view = output
+            .texture
+            .create_view(&wgpu::TextureViewDescriptor::default());
+        let mut encoder = self
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                label: Some("Render Encoder"),
+            });
+        {
+            let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                label: Some("Render Pass"),
+                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                    view: &view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(wgpu::Color::BLUE),
+                        store: true,
+                    },
+                })],
+                depth_stencil_attachment: None,
+            });
+
+            self.main_display.render(&mut render_pass);
+        }
+
+        self.main_display.update_buffers(&self.queue);
+        self.queue.submit(std::iter::once(encoder.finish()));
+        output.present();
+        Ok(())
+    }
+}
+
+struct MainDisplay {
+    pipeline: RenderPipeline,
+    time_start: Instant,
+    fft: Arc<Mutex<Vec<f32>>>,
+    time_buffer: Buffer,
+    fft_buffer: Buffer,
+    vertices: Buffer,
+    bind_group: BindGroup,
+}
+
+impl MainDisplay {
+    fn new(
+        fft: Arc<Mutex<Vec<f32>>>,
+        device: &Device,
+        fragment_shader: &str,
+        format: TextureFormat,
+    ) -> MainDisplay {
         let vertex_shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
             label: Some("vertex_shader"),
             source: wgpu::ShaderSource::Glsl {
-                shader: vertex_shader_s.into(),
+                shader: include_str!("shader.vert").into(),
                 stage: naga::ShaderStage::Vertex,
                 defines: naga::FastHashMap::default(),
             },
@@ -190,7 +250,7 @@ impl State {
         let fragment_shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
             label: Some("fragment_shader"),
             source: wgpu::ShaderSource::Glsl {
-                shader: fragment_shader_s.into(),
+                shader: fragment_shader.into(),
                 stage: naga::ShaderStage::Fragment,
                 defines: naga::FastHashMap::default(),
             },
@@ -218,51 +278,20 @@ impl State {
                     },
                     count: None,
                 },
-                // wgpu::BindGroupLayoutEntry {
-                //     binding: 2,
-                //     visibility: wgpu::ShaderStages::FRAGMENT,
-                //     ty: wgpu::BindingType::Buffer {
-                //         ty: wgpu::BufferBindingType::Storage { read_only: true },
-                //         has_dynamic_offset: false,
-                //         min_binding_size: None,
-                //     },
-                //     count: None,
-                // },
-                // wgpu::BindGroupLayoutEntry {
-                //     binding: 3,
-                //     visibility: wgpu::ShaderStages::FRAGMENT,
-                //     ty: wgpu::BindingType::Buffer {
-                //         ty: wgpu::BufferBindingType::Storage { read_only: true },
-                //         has_dynamic_offset: false,
-                //         min_binding_size: None,
-                //     },
-                //     count: None,
-                // },
             ],
         });
-        let time_buffer = create_time_buffer(&device, 0.0);
+        let time_buffer = create_time_buffer(device, 0.0);
         let fft_lock = fft.lock().unwrap();
-        let fft_buffer = create_fft_buffer(&device, fft_lock.as_slice());
+        let fft_buffer = create_fft_buffer(device, fft_lock.as_slice());
         drop(fft_lock);
-        let bind_group = create_bind_group(&device, &bind_group_layout, &time_buffer, &fft_buffer);
+        let bind_group = create_bind_group(device, &bind_group_layout, &time_buffer, &fft_buffer);
         let render_pipeline_layout =
             device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
                 label: Some("render pipeline layout"),
                 bind_group_layouts: &[&bind_group_layout],
                 push_constant_ranges: &[],
             });
-        let render_pipeline =
-            device.create_render_pipeline(&render_pipeline::render_pipeline_descriptor(
-                &vertex_shader,
-                &fragment_shader,
-                &render_pipeline_layout,
-                &[Some(wgpu::ColorTargetState {
-                    format: config.format,
-                    blend: Some(wgpu::BlendState::REPLACE),
-                    write_mask: wgpu::ColorWrites::ALL,
-                })],
-                &[Vertex::desc()],
-            ));
+        let vertices = Vertex::square();
         let mut vertex_bytes = vec![];
         let mut vertex_bytes_writer = crevice::std430::Writer::new(&mut vertex_bytes);
         vertex_bytes_writer
@@ -273,81 +302,48 @@ impl State {
             contents: &vertex_bytes[..],
             usage: wgpu::BufferUsages::VERTEX,
         });
-
+        let pipeline = device.create_render_pipeline(&render_pipeline::render_pipeline_descriptor(
+            &vertex_shader,
+            &fragment_shader,
+            &render_pipeline_layout,
+            &[Some(wgpu::ColorTargetState {
+                format,
+                blend: Some(wgpu::BlendState::REPLACE),
+                write_mask: wgpu::ColorWrites::ALL,
+            })],
+            &[Vertex::desc()],
+        ));
         Self {
-            surface,
-            device,
-            queue,
-            config,
-            size,
-            render_pipeline,
-            vertex_buffer,
-            bind_group,
-            num_vertices: vertices.len(),
+            pipeline,
+            time_start: Instant::now(),
+            fft,
             time_buffer,
             fft_buffer,
+            vertices: vertex_buffer,
+            bind_group,
         }
     }
 
-    fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
-        if new_size.width > 0 && new_size.height > 0 {
-            self.size = new_size;
-            self.config.width = self.size.width;
-            self.config.height = self.size.height;
-            self.surface.configure(&self.device, &self.config);
-        }
+    fn render<'a, 'b: 'a>(&'b self, render_pass: &mut RenderPass<'a>) {
+        render_pass.set_pipeline(&self.pipeline);
+        render_pass.set_vertex_buffer(0, self.vertices.slice(..));
+        render_pass.set_bind_group(0, &self.bind_group, &[]);
+        render_pass.draw(0..6, 0..1);
     }
 
-    fn input(&mut self, _event: &WindowEvent) -> bool {
-        false
-    }
-
-    fn render(
-        &mut self,
-        duration: Duration,
-        fft: &Arc<Mutex<Vec<f32>>>,
-    ) -> Result<(), wgpu::SurfaceError> {
-        let output = self.surface.get_current_texture()?;
-        let view = output
-            .texture
-            .create_view(&wgpu::TextureViewDescriptor::default());
-        let mut encoder = self
-            .device
-            .create_command_encoder(&wgpu::CommandEncoderDescriptor {
-                label: Some("Render Encoder"),
-            });
-        {
-            let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-                label: Some("Render Pass"),
-                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
-                    view: &view,
-                    resolve_target: None,
-                    ops: wgpu::Operations {
-                        load: wgpu::LoadOp::Clear(wgpu::Color::BLUE),
-                        store: true,
-                    },
-                })],
-                depth_stencil_attachment: None,
-            });
-
-            render_pass.set_pipeline(&self.render_pipeline);
-            render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
-            render_pass.set_bind_group(0, &self.bind_group, &[]);
-            render_pass.draw(0..self.num_vertices as u32, 0..1);
-        }
+    fn update_buffers(&self, queue: &Queue) {
         let mut bytes = vec![];
         let mut sphere_bytes_writer = crevice::std430::Writer::new(&mut bytes);
-        sphere_bytes_writer.write(&duration.as_secs_f32()).unwrap();
-        self.queue.write_buffer(&self.time_buffer, 0, &bytes);
+        sphere_bytes_writer
+            .write(&self.time_start.elapsed().as_secs_f32())
+            .unwrap();
+        queue.write_buffer(&self.time_buffer, 0, &bytes);
         let mut bytes = vec![];
         let mut sphere_bytes_writer = crevice::std430::Writer::new(&mut bytes);
-        let fft_lock = fft.lock().unwrap();
+        let fft_lock = self.fft.lock().unwrap();
         sphere_bytes_writer.write(fft_lock.as_slice()).unwrap();
         drop(fft_lock);
-        self.queue.write_buffer(&self.fft_buffer, 0, &bytes);
-        self.queue.submit(std::iter::once(encoder.finish()));
-        output.present();
-        Ok(())
+        queue.write_buffer(&self.fft_buffer, 0, &bytes);
     }
 }
 
